982ba2d3ba932b5337363e2162b796e6088b4c22
Nick Brennan <nick_p_b@yahoo.com>
2016-03-02 11:49:59 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// server.js
+// load the things we need
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+	var drinks = [
+        { name: 'Bloody Mary', drunkness: 3 },
+        { name: 'Martini', drunkness: 5 },
+        { name: 'Scotch', drunkness: 10 }
+    ];
+    res.render('pages/index', {
+    	foo: 'bar',
+    	drinks: drinks
+    });
+});
+
+// about page 
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+app.listen(8080);
+console.log('8080 is the magic port');
